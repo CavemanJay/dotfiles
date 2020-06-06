b6056d1ee027738e652ff59eba0ebe7a941b08d7
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-06-06 12:53:47 -0400

    Rice qtile bar

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,6 +4,7 @@ import socket
 
 from keys import keys, mod, alt
 from layouts import *
+from appearance import *
 
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.lazy import lazy
@@ -26,45 +27,6 @@ for i in groups:
         # Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
     ])
 
-##### COLORS #####
-colors = [["#282a36", "#282a36"],  # panel background
-          ["#434758", "#434758"],  # background for current screen tab
-          ["#ffffff", "#ffffff"],  # font color for group names
-          ["#ff5555", "#ff5555"],  # border line color for current tab
-          ["#8d62a9", "#8d62a9"],  # border line color for other tab and odd widgets
-          ["#668bd7", "#668bd7"],  # color for the even widgets
-          ["#e1acff", "#e1acff"]]  # window name
-
-##### PROMPT #####
-prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
-
-widget_defaults = dict(
-    font="Ubuntu Mono",
-    fontsize=22,
-    padding=3,
-    #background=colors[2]
-)
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.TextBox("default config", name="default"),
-                widget.Systray(),
-                widget.Net(interface='wlp59s0'),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.Battery(),
-                widget.QuickExit(),
-            ],
-            24,
-        ),
-    ),
-]
 
 
 dgroups_key_binder = None
