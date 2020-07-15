864765356f94aa50fa4956070138130d8a90be31
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-07-15 19:26:00 -0400

    Update wallpaper scripts

--- scripts/dmenu/dmenu-set-wallpaper.sh
+++ scripts/dmenu/dmenu-set-wallpaper.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 declare options=$(ls $HOME/Sync/Backgrounds)
 
-choice=$(echo -e "${options[@]}" | dmenu -i -c -l 20 -p 'Choose wallpaper folder: ')
+choice=$(echo -e "${options[@]}" | dmenu -i -p 'Choose wallpaper folder: ')
 
 echo "$HOME/Sync/Backgrounds/$choice" > $HOME/scripts/wallpaper/current
+echo 0 > $HOME/scripts/wallpaper/safemode
 
 source $HOME/scripts/wallpaper/set_wallpaper.sh
