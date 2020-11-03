a6ac098683efd06952d6fccdb9741db4bbc4c3dd
Jay <Jordan.Cuevas@precisecal.com>
2020-11-03 12:53:42 -0500

    Update scripts

--- scripts/wallpaper/safe_wallpaper.sh
+++ scripts/wallpaper/safe_wallpaper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 root="$HOME/Pictures/wallpapers"
-WALLPAPER=$(ls $root | shuf -n 1)
-feh --bg-fill "$root/$WALLPAPER"
-echo "$WALLPAPER" > $HOME/scripts/wallpaper/current
+feh --randomize --bg-fill $root
+
 echo 1 > $HOME/scripts/wallpaper/safemode
--- scripts/wallpaper/set_wallpaper.sh
+++ scripts/wallpaper/set_wallpaper.sh
@@ -1,18 +1,35 @@
 #!/bin/bash -x
 
+is_safemode(){
+    safe=$(cat $HOME/scripts/wallpaper/safemode)
+    return $([[ $safe -eq 1 ]])
+}
+
 safepaper() { 
     local WALLPAPER=$(ls -d $HOME/Pictures/wallpapers/* | shuf -n 1)
     feh --bg-fill $WALLPAPER
     echo "$WALLPAPER" > $HOME/scripts/wallpaper/current
 }
 
-unsafe() {
-    FOLDER=$(cat $HOME/scripts/wallpaper/current)
-    WALLPAPER=$(ls "$FOLDER" | grep -E "png|jpg" | shuf -n 1)
-    WALLPAPER="$FOLDER/$WALLPAPER" 
-    feh --bg-fill "$WALLPAPER"
+get_current_feh(){
+    echo $(grep -oE "'.*'" ~/.fehbg | sed "s/'//g")
 }
 
-SAFE=$(cat $HOME/scripts/wallpaper/safemode)
+get_current(){
+    current=$(cat ~/scripts/wallpaper/current)
+    current=$(basename "$current")
+    echo $current
+}
 
-[[ SAFE -eq 1 ]] && safepaper || unsafe
+unsafe() {
+    folder=$(cat ~/scripts/wallpaper/current)
+#   WALLPAPER=$(ls "$FOLDER" | grep -E "png|jpg" | shuf -n 1)
+#   WALLPAPER="$FOLDER/$WALLPAPER" 
+    feh --randomize --bg-fill "$folder"
+}
+
+
+is_safemode && safepaper || unsafe
+is_safemode && basename `get_current_feh` > ~/scripts/wallpaper/current
+
+
