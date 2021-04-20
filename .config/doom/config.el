7370a5f6fc8e3ae45fec8fa5915314d0b87569a8
Jay <Jordan.Cuevas@precisecal.com>
2021-04-20 19:24:44 -0400

    Update doom emacs config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -53,3 +53,29 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(map! :leader
+      (:prefix ("d" . "dired")
+       :desc "Open dired" "d" #'dired
+       :desc "Dired jump to current" "j" #'dired-jump)
+       (:after dired
+        (:map dired-mode-map
+         :desc "Peep-dired image previews" "d p" #'peep-dired
+         :desc "Dired view file" "d v" #'dired-view-file)))
+
+;; Make 'h' and 'l' go back and forward in dired. Much faster to navigate the directory structure!
+(evil-define-key 'normal dired-mode-map
+  (kbd "h") 'dired-up-directory
+  ;(kbd "l") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
+  (kbd "l") 'dired-find-file) ; use dired-find-file instead if not using dired-open package
+;; If peep-dired is enabled, you will get image previews as you go up/down with 'j' and 'k'
+(evil-define-key 'normal peep-dired-mode-map
+  (kbd "j") 'peep-dired-next-file
+  (kbd "k") 'peep-dired-prev-file)
+(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
+
+;; Get file icons in dired
+;(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+
+(after! org
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
